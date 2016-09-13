4245306ba1841cac047c0a6c4258312af85e4d58
rdrew <rdrew@libprojects.upei.ca>
2016-09-13 15:01:37 -0300

    added ui-kit

--- js/vre_upei.js
+++ js/vre_upei.js
@@ -6,9 +6,43 @@
 
 			$('.pager').addClass('uk-pagination');
 			$('#tasks a').addClass('uk-button');
+			$('input#edit-submit').addClass('uk-button');
 		}
 
 	};
+
+	Drupal.behaviors.flipster = {
+
+		attach: function (context, settings) {
+
+			$('.collection_slideshow').flipster({
+
+				style: 'carousel',
+			spacing: -0.5,
+			buttons: true,
+
+
+			});
+
+			$("#coverflow").flipster({
+				style: 'carousel',
+				spacing: -0.5,
+				nav: true,
+				buttons: true,
+			});
+
+		}
+
+	};
+	Drupal.behaviors.colorbox = {
+
+		attach: function (context, settings) {
+
+			$('.islandora-basic-image-content a').colorbox({maxWidth:"80%"});
+		}
+
+	};
+
 
 	//$('.form-item-add-bookmarks select', context).once( function() {
 	//$(this).hide();
